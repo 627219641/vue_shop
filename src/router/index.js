b91b7f559c941ec6b6536aac7fae80f0e32fd6fe
627219641@qq.com <627219641@qq.com>
2023-02-06 15:11:41 +0800

    登录功能提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '@/components/Login.vue'
+import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/Login' },
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login', component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/home', component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // 如果用戶访问登录页面，直接放行
+  if (to.path === '/login') {
+    next()
+  }
+  const token = window.sessionStorage.getItem('token')
+  if (token) {
+    next()
+  } else {
+    next('/login')
+  }
 })
 
 export default router
